384b80ebac7c0e9bdb78e28ec80269290a5e5296
Andrii Yurchuk <ay@mntw.re>
2021-05-08 17:29:28 +0200

    Make the code a little nicer

--- gol.py
+++ gol.py
@@ -1,83 +1,130 @@
 import copy
+import itertools
 import time
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Tuple
 
 from rich.box import ROUNDED
 from rich.console import Console
 from rich.table import Table
 
-CELL = "\u25a9"
+SPACE_VACANT = " "
+SPACE_POPULATED = "\u25a9"
 BOARD_SIZE = (25, 25)
-BOARD = {(x, y): " " for x in range(BOARD_SIZE[0]) for y in range(BOARD_SIZE[1])}
-INITIAL_STATE = [
-    (12, 11),
-    (13, 12),
-    (11, 13),
-    (12, 13),
-    (13, 13),
-]
 
 
-def draw_seed(board, initial_state):
-    for c in initial_state:
-        board[c] = CELL
-    draw_board(board)
+@dataclass
+class Location:
+    x: int
+    y: int
+
+    def __hash__(self):
+        return hash(f"{self.x,self.y}")
 
 
-def draw_board(board):
-    table = Table(show_header=False, show_lines=True, box=ROUNDED)
-    for y in range(BOARD_SIZE[1]):
-        table.add_row(*[str(board[(x, y)]) for x in range(BOARD_SIZE[0])])
-    console = Console()
-    console.print(table)
+@dataclass
+class Space:
+    is_populated: bool = False
+
+    def __str__(self):
+        return SPACE_POPULATED if self.is_populated else SPACE_VACANT
+
+    def vacate(self):
+        self.is_populated = False
+
+    def populate(self):
+        self.is_populated = True
 
 
-def get_adjacent_spaces_coords(space):
+@dataclass
+class Board:
+    size: Tuple[int, int]
+    data: Dict[Location, Space]
+
+    @classmethod
+    def create(
+        cls, size: Tuple[int, int], data: Optional[Dict[Location, Space]] = None
+    ) -> "Board":
+        board = cls(size=size, data={})
+        for x, y in itertools.product(range(size[0]), range(size[1])):
+            location = Location(x, y)
+            space = Space()
+            if data is not None and data.get(location, Space()).is_populated:
+                space.populate()
+            board.data[location] = space
+        return board
+
+    def get_adjacent_spaces(self, location: Location) -> List[Space]:
+        adjacent_locations = get_adjacent_locations(location)
+        adjacent_spaces = []
+        for location in adjacent_locations:
+            try:
+                adjacent_spaces.append(self.data[location])
+            except KeyError:
+                # we are out of range of the board
+                pass
+        return adjacent_spaces
+
+    def get_num_adjacent_populated_spaces(self, location: Location) -> int:
+        num_adjacent_spaces = 0
+        for space in self.get_adjacent_spaces(location):
+            if space.is_populated:
+                num_adjacent_spaces += 1
+        return num_adjacent_spaces
+
+    def evolve(self) -> Dict[Location, Space]:
+        # TODO: Make it work without copying
+        data = copy.deepcopy(self.data)
+        for location, space in self.data.items():
+            num_adjacent_spaces = self.get_num_adjacent_populated_spaces(location)
+            if space.is_populated:
+                if num_adjacent_spaces < 2:
+                    # underpopulation
+                    data[location].vacate()
+                if num_adjacent_spaces > 3:
+                    # overpopulation
+                    data[location].vacate()
+            else:
+                if num_adjacent_spaces == 3:
+                    data[location].populate()
+        return data
+
+    def render(self) -> None:
+        table = Table(show_header=False, show_lines=True, box=ROUNDED)
+        for y in range(self.size[1]):
+            row = [str(self.data[Location(x, y)]) for x in range(self.size[0])]
+            table.add_row(*row)
+        console = Console()
+        console.print(table)
+
+
+def get_adjacent_locations(location: Location) -> List[Location]:
     return [
-        (space[0] - 1, space[1] - 1),
-        (space[0], space[1] - 1),
-        (space[0] + 1, space[1] - 1),
-        (space[0] - 1, space[1]),
-        (space[0] + 1, space[1]),
-        (space[0] - 1, space[1] + 1),
-        (space[0], space[1] + 1),
-        (space[0] + 1, space[1] + 1),
+        Location(location.x - 1, location.y - 1),
+        Location(location.x, location.y - 1),
+        Location(location.x + 1, location.y - 1),
+        Location(location.x - 1, location.y),
+        Location(location.x + 1, location.y),
+        Location(location.x - 1, location.y + 1),
+        Location(location.x, location.y + 1),
+        Location(location.x + 1, location.y + 1),
     ]
 
 
-def get_num_adjacent_cells(space, board):
-    num_adjacent_cells = 0
-    for coord in get_adjacent_spaces_coords(space):
-        try:
-            if board[coord] == CELL:
-                num_adjacent_cells += 1
-        except KeyError:
-            # we are out of range of the board
-            pass
-    return num_adjacent_cells
-
-
-def draw_generation(board):
-    new_board = copy.copy(board)
-    for space, population in board.items():
-        num_adjacent_cells = get_num_adjacent_cells(space, board)
-        if population == CELL:
-            if num_adjacent_cells < 2:
-                # die (underpopulation)
-                new_board[space] = " "
-            if num_adjacent_cells > 3:
-                # die (overpopulation)
-                new_board[space] = " "
-        else:
-            if num_adjacent_cells == 3:
-                # become a cell
-                new_board[space] = CELL
-    draw_board(new_board)
-    return new_board
-
-
 if __name__ == "__main__":
-    draw_seed(BOARD, INITIAL_STATE)
-    board = BOARD
-    for i in range(100):
-        board = draw_generation(board)
+    glider = {
+        Location(12, 11): Space(True),
+        Location(13, 12): Space(True),
+        Location(11, 13): Space(True),
+        Location(12, 13): Space(True),
+        Location(13, 13): Space(True),
+    }
+
+    board = Board.create(BOARD_SIZE, glider)
+    board.render()
+
+    for i in range(50):
+        data = board.evolve()
+        board = Board.create(BOARD_SIZE, data)
+        board.render()
         time.sleep(0.25)
